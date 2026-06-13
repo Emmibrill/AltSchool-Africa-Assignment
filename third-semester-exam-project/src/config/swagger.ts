30b8bee8801f327c9f290fa7c19d5d7f2066d685
Emmanue Okokon Udo <ubryliant@gmail.com>
2026-06-13 21:01:45 +0100

    update package.json

--- third-semester-exam-project/src/config/swagger.ts
+++ third-semester-exam-project/src/config/swagger.ts
@@ -1,20 +1,48 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
-// Swagger definition
 export const swaggerSpec = swaggerJSDoc({
   definition: {
     openapi: "3.0.0",
+
     info: {
       title: "Eventful API",
       version: "1.0.0",
-      description: "Event Ticketing Platform API Documentation",
+      description:
+        "Eventful Event Ticketing Platform API",
     },
+
     servers: [
       {
         url: "http://localhost:5000",
+        description: "Local Development",
+      },
+      {
+        url:
+          process.env.RENDER_EXTERNAL_URL ||
+          "https://altschool-africa-assignment-ymm0.onrender.com",
+        description: "Production",
+      },
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+
+    security: [
+      {
+        bearerAuth: [],
       },
     ],
   },
 
-  apis: ["src/modules/**/*.ts", "src/**/*.routes.ts"],
+  apis: [
+    "./src/**/*.routes.ts",
+    "./src/**/*.controller.ts",
+  ],
 });
